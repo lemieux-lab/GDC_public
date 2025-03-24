307688296136aaf55c370162bef1e609fe9bf1d6
Carl Munoz <CarlMunoz1029>
2025-03-24 16:43:27 -0400

    start pam50 process

--- data_processing.jl
+++ data_processing.jl
@@ -31,3 +31,52 @@ create_tgca_fetch_data_file("gdc_sample_sheet.2025-03-21.tsv", "GDC_raw")
 
 
 
+
+
+using HDF5
+function load_tcga_data(fname)
+
+    fid = h5open(fname, "r")
+
+    data = read(fid, "data_matrix")
+    samples = read(fid, "case_id")
+    genes = read(fid, "gene_id")
+    gene_type = read(fid, "gene_type")
+    cancer_types = read(fid, "cancer_type")
+
+    close(fid)
+
+    return data, samples, genes, gene_type, cancer_types
+end
+
+data,samples, genes, gene_type, cancer_types = load_tcga_data("full_TCGA_GE.h5")
+
+
+data
+
+
+cancer_idx = cancer_types .== "TCGA-BRCA"
+
+function cancer_subset(data, samples, genes, gene_type, cancer_types, target_type, outfile)
+    cancer_idx = cancer_types .== target_type
+
+    fid = h5open(outfile, "w")
+
+    fid["data_matrix"] = data[cancer_idx, : ]
+    fid["samples"] = samples[cancer_idx]
+    fid["gene_id"] = genes
+    fid["gene_type"] = gene_type
+    fid["cancer_type"] = cancer_types[cancer_idx]
+
+    close(fid)
+end
+
+cancer_subset(data,samples, genes, gene_type, cancer_types, "TCGA-BRCA", "BRCA_TCGA_GE.h5")
+
+#TODO: add this to the cancer_subset h5 for PAM50
+pam50 = CSV.read("mmc2.csv", DataFrame)
+pam50_labels = pam50[!,3]
+count_map = Dict([(label, sum(pam50_labels .== label)) for label in unique(pam50_labels)])
+
+
+length(pam50_labels)
